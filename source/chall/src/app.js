34803ce2115a62433e1a99db3ea71f348d1afb0b
Anis <anis.lounis@concordia.ca>
2024-11-12 15:32:24 -0500

    Added twig and an initial card template

--- source/chall/src/app.js
+++ source/chall/src/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const twig = require('twig');
+
+// Register Twig as the rendering engine for .twig files
+app.engine('twig', twig.__express);
+
+// Set up Express to use Twig as the template engine
+app.set('view engine', 'twig');
+// Set the directory for template files
+app.set('views', path.join(__dirname, 'templates'));
 
 // Port 2025
-const port = 2025;
+const port = 2024;
 
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,12 +22,16 @@ app.use(express.urlencoded({extended: true}));
 
 // Index
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'private', 'index.html'));
+    const data = {
+        title: 'Home Page',
+        message: 'Welcome to my website!'
+    };
+    res.render('index.html.twig', data);
 });
 
 // 404 Not Found handler for all routes that don't match
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'private', '404.html'));
+    res.render('404.html.twig');
 });
 
 // Start the server
